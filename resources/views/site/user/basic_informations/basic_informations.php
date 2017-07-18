d13c7913fc629a9c1ada5f986ec489ad0814864a
toutche <contato@toutche.com.br>
2017-07-18 16:51:15 -0300

    Complemento perfil (Views)
    Criação dos diretório user->basic_informations, separação do código
    (View) de informações básicas e criação do campo celular.

---
+++ resources/views/site/user/basic_informations/basic_informations.php
@@ -0,0 +1,57 @@
+<div class="divider"><h3>Informações Básicas</h3></div>
+	<div class="form-group">
+		<label class="control-label" for="textarea">Nome</label>
+		<input type="text" class="form-control" placeholder="Nome">
+	</div>
+	<div class="form-group">
+		<label class="control-label" for="textarea">CPF</label>
+		<input type="text" class="form-control" placeholder="cpf">
+	</div>
+	<div class="form-group">
+		<label class="control-label" for="textarea"></label>
+		<label class="control-label" for="textarea">E-mail</label>
+		<input type="text" class="form-control" placeholder="seu@dominio.com.br">
+	</div>
+	<div class="form-group">
+		<label class="control-label" for="textarea">Título profissional</label>
+		<input type="text" class="form-control" placeholder="Ex: Engenheiro...">
+	</div>
+	<div class="form-group">
+		<div class="row">
+			<div class="col-md-6">
+				<label class="control-label" for="textarea">Telefone</label>
+				<input type="text" class="form-control" placeholder="(51) xxxxxxxxx">
+			</div>
+			<div class="col-md-6">
+				<label class="control-label" for="textarea">Celular</label>
+				<input type="text" class="form-control" placeholder="(51) xxxxxxxxx">
+			</div>
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="control-label" for="textarea">Site</label>
+		<input type="text" class="form-control" placeholder="www.exemplo.com.br">
+	</div>
+	<div class="form-group">
+		<label class="control-label" for="textarea">Data de Nascimento</label>
+		<input type="text" class="form-control" placeholder="dd/mm/aaaa">
+	</div>
+	<div class="form-group">
+		<label class="control-label" for="textarea">Sobre mim</label>
+	</div>
+	<section class="editor">
+		<div class="edit">
+			
+		</div>
+	</section>
+	<div class="form-group">
+		<div class="button-group">
+			<div class="action-buttons">
+				<div class="upload-button">
+					<!--button class="btn btn-common">Choose a cover image</button-->
+					<input type="file" name="file" id="file" class="inputfile" />
+					<label for="file">Escolha imagem de perfil</label>
+				</div>
+			</div>
+		</div>
+	</div>
