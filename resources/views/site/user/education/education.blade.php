bc6aa3286193c0e34d2ee4d2420c0fa92fd402d7
toutche <contato@toutche.com.br>
2017-07-18 17:01:04 -0300

    Complemento Perfil (Views)
    Criação do diretório education, separação do código
    (View) de education e alteração do nome do arquivo
    basic_informations.php para basic_informations.blade.php

---
+++ resources/views/site/user/education/education.blade.php
@@ -0,0 +1,93 @@
+<div class="divider"><h3>Educação</h3></div>
+<div class="form-group">
+	<label class="control-label" for="textarea">Grau</label>
+	<div>
+		<label class="styled-select degree">
+			<select id="options-degree" class="dropdown-product selectpicker">
+				<option value="select-degree">Selecione uma opção</option>
+				<option value="graduating">Graduando</option>
+				<option value="graduate">Graduado</option>
+			</select>
+		</label>
+	</div>
+</div>
+<div class="form-group">
+	<label class="control-label" for="textarea">Curso</label>
+	<input type="text" class="form-control" placeholder="Curso">
+</div>
+<div class="form-group" id="semestre">
+	<label class="control-label" for="textarea">Semestre</label>
+	<input type="text" class="form-control" placeholder="Ex: 6°">
+</div>
+<div class="add-post-btn" id="crea">
+	<div class="pull-left" id="crea-numero">
+		<div class="form-group">
+			<label class="control-label" for="textarea">Estado</label>
+			<div class="search-category-container">
+				<label class="styled-select state">
+					<select class="dropdown-product selectpicker">
+						<option value="estado">Selecione o Estado</option> 
+						<option value="ac">Acre</option> 
+						<option value="al">Alagoas</option> 
+						<option value="am">Amazonas</option> 
+						<option value="ap">Amapá</option> 
+						<option value="ba">Bahia</option> 
+						<option value="ce">Ceará</option> 
+						<option value="df">Distrito Federal</option> 
+						<option value="es">Espírito Santo</option> 
+						<option value="go">Goiás</option> 
+						<option value="ma">Maranhão</option> 
+						<option value="mt">Mato Grosso</option> 
+						<option value="ms">Mato Grosso do Sul</option> 
+						<option value="mg">Minas Gerais</option> 
+						<option value="pa">Pará</option> 
+						<option value="pb">Paraíba</option> 
+						<option value="pr">Paraná</option> 
+						<option value="pe">Pernambuco</option> 
+						<option value="pi">Piauí</option> 
+						<option value="rj">Rio de Janeiro</option> 
+						<option value="rn">Rio Grande do Norte</option> 
+						<option value="ro">Rondônia</option> 
+						<option value="rs">Rio Grande do Sul</option> 
+						<option value="rr">Roraima</option> 
+						<option value="sc">Santa Catarina</option> 
+						<option value="se">Sergipe</option> 
+						<option value="sp">São Paulo</option> 
+						<option value="to">Tocantins</option> 
+					</select>
+				</label>
+			</div>
+		</div>
+	</div>
+	<div class="pull-left" id="crea-estado">
+		<div class="form-group">
+			<label class="control-label" for="textarea">Número do CREA</label>
+			<input type="text" class="form-control" placeholder="Ex: 123456">
+		</div>
+	</div>
+	<div class="clear"> </div>
+</div>
+<div class="form-group">
+	<label class="control-label" for="textarea">Instituição de ensino</label>
+	<input type="text" class="form-control" placeholder="Ex: UniRitter">
+</div>
+<div class="form-group">
+	<div class="row">
+		<div class="col-md-6">
+			<label class="control-label" for="textarea">Inicio</label>
+			<input type="text" class="form-control" placeholder="Ex: 2014">
+		</div>
+		<div class="col-md-6">
+			<label class="control-label" for="textarea">Término</label>
+			<input type="text" class="form-control" placeholder="Ex: 2018">
+		</div>
+	</div>
+</div>
+<div class="add-post-btn">
+	<div class="pull-left">
+		<a href="#" class="btn-added"><i class="ti-plus"></i> Adicionar</a>
+	</div>
+	<div class="pull-right">
+		<a href="#" class="btn-delete"><i class="ti-trash"></i> Deletar este</a>
+	</div>
+</div>
