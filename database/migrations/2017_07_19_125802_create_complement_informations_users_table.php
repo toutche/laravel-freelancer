625dd024f342877aa801e0fff4b224c8541eaa5c
toutche <contato@toutche.com.br>
2017-07-19 10:21:48 -0300

    Criação de Migração
    Migration complement_informations_users_table

---
+++ database/migrations/2017_07_19_125802_create_complement_informations_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComplementInformationsUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('complement_informations_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('professional_title')->nullable();
+            $table->string('site')->nullable();
+            $table->longText('about_me')->nullable();
+            $table->binary('profile_image')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('complement_informations_users');
+    }
+}
