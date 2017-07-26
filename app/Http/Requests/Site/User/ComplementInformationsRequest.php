3d84ce44b308f165447c53faf7556152fe77e379
toutche <contato@toutche.com.br>
2017-07-26 18:29:30 -0300

    Ajuste de validações
    Ajuste de validações PHP nas informações básicas do complemento perfil.

--- app/Http/Requests/Site/User/ComplementInformationsRequest.php
+++ app/Http/Requests/Site/User/ComplementInformationsRequest.php
@@ -21,6 +21,28 @@ class ComplementInformationsRequest extends FormRequest
             },
             'O campo CPF tem que possuir 11 dígitos'
         );
+
+        $validationFactory->extend(
+            'phone',
+            function ($attribute, $value, $parameters) {
+                if(strlen(str_replace(['(', ')','-',' '], "", $value)) == 10) 
+                    return true;
+                else
+                    return false;
+            },
+            'O campo telefone tem que possuir 10 dígitos'
+        );
+
+        $validationFactory->extend(
+            'cell_phone',
+            function ($attribute, $value, $parameters) {
+                if(strlen(str_replace(['(', ')','-',' '], "", $value)) == 10 || strlen(str_replace(['(', ')','-',' '], "", $value)) == 11) 
+                    return true;
+                else
+                    return false;
+            },
+            'O campo celular tem que possuir 10 ou 11 dígitos'
+        );
     }
 
     //Fix Bug on the tag <br> to editor Summernote
@@ -58,12 +80,12 @@ class ComplementInformationsRequest extends FormRequest
             'cpf'                      => 'required|cpf',
             'email'                    => 'required|email|max:40|unique:users',
             'professional_title'       => 'min:3|max:100|nullable',
-            'phone'                    => 'min:10|max:20|nullable',
-            'cell_phone'               => 'min:10|max:20|nullable',
+            'phone'                    => 'phone|nullable',
+            'cell_phone'               => 'cell_phone|nullable',
             'site'                     => 'url|nullable',
             'date_birth'               => 'date_format:d/m/Y',
             'about_me'                 => 'string|max:1500|nullable',
-            'image_perfil'             => 'image|mimes:jpeg,png,jpg|max:5000|nullable'
+            'image_perfil'             => 'image|mimes:jpeg,png,jpg|max:2000|nullable'
         ];
     }
 
@@ -74,21 +96,17 @@ class ComplementInformationsRequest extends FormRequest
             'name.min'                      => 'Mínimo de caracteres para o nome é 3',
             'name.max'                      => 'Máximo de caracteres para o nome é 100',
             'cpf.required'                  => 'O campo CPF é obrigatório',
-            'email.required'                => 'O campo email é obrigatório',
+            'email.required'                => 'O campo e-mail é obrigatório',
             'email.email'                   => 'Digite um e-mail válido',
             'email.max'                     => 'Máximo de caracteres para o email é 40',
             'email.unique'                  => 'Este e-mail já está registrado',
             'professional_title.min'        => 'Mínimo de caracteres para o título profissional é 3',
             'professional_title.max'        => 'Máximo de caracteres para o título profissional é 100',
-            'phone.min'                     => 'Mínimo de caracteres para o telefone é 3',
-            'phone.max'                     => 'Máximo de caracteres para o telefone é 20',
-            'cell_phone.min'                => 'Mínimo de caracteres para o celular é 3',
-            'cell_phone.max'                => 'Máximo de caracteres para o celular é 20',
             'site.url'                      => 'Digite uma url no formato http://www ou https://www',
-            'date_birth.date_format'        => 'Digite uma data no formato DD/MM/AAAA',
+            'date_birth.date_format'        => 'Digite uma data válida no formato DD/MM/AAAA',
             'about_me.max'                  => 'Máximo de caracteres para o sobre mim é 1500',
             'image_perfil.mimes'            => 'Insira uma imagem no formato jpeg, png ou jpg',
-            'image_perfil.max'              => 'Tamanho máximo para imagem de perfil é 5MB',
+            'image_perfil.max'              => 'Tamanho máximo para imagem de perfil é 2MB',
         ];
     }
 }
