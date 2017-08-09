030ebdcc9d670c85cd547318c409619274bd7316
toutche <contato@toutche.com.br>
2017-08-09 10:34:09 -0300

    Validação PHP da educação
    Validação dos dados em PHP da educação em ComplementInformationsRequest
    e exibição dos erros na education.blade. Mudança no id de crea-estado
    para crea-number, isso no main.css, para afastar o número do crea do
    estado do crea.

--- app/Http/Requests/Site/User/ComplementInformationsRequest.php
+++ app/Http/Requests/Site/User/ComplementInformationsRequest.php
@@ -30,7 +30,7 @@ class ComplementInformationsRequest extends FormRequest
                 else
                     return false;
             },
-            'O campo :attribute não possui o número de caracteres correpondentes'
+            'O campo :attribute não possui o número de caracteres correspondentes'
         );
 
         $validationFactory->extend(
@@ -86,7 +86,8 @@ class ComplementInformationsRequest extends FormRequest
      */
     public function rules()
     {
-        return [
+        $rules = array();
+        $rules = [
             'name'                          => 'required|min:3|max:100',
             'cpf'                           => 'required|cpf',
             'email'                         => 'required|email|max:40|unique:users',
@@ -97,17 +98,36 @@ class ComplementInformationsRequest extends FormRequest
             'date_birth'                    => 'required|date_format:d/m/Y',
             'about_me'                      => 'string|max:1500|nullable',
             'image_perfil'                  => 'image|mimes:jpeg,png,jpg|max:2000|nullable',
+            //Educations
+            'ed_select_degree_.*'           => 'required|in:graduating,graduate',
+            'ed_course_.*'                  => 'required|min:3|max:100',
+            'ed_college_.*'                 => 'required|min:3|max:100',
+            'ed_start_date_.*'              => 'required|numeric|exactly:4',
+            'ed_end_date_.*'                => 'numeric|exactly:4', 
             //Experiences
             'ex_company_name_.*'            => 'required|min:3|max:100',
             'ex_responsibility_name_.*'     => 'required|min:3|max:100',
             'ex_start_date_.*'              => 'numeric|exactly:4',
             'ex_end_date_.*'                => 'numeric|exactly:4'
         ];
+        //As graduation selection applies the respective rules
+        for($i = 1; $i <= count(Input::get('ed_select_degree_.*')); $i++ ) {
+            if(!empty(Input::get('ed_select_degree_.' . $i ))) {
+                if(Input::get('ed_select_degree_.' . $i) == "graduating") {
+                    $rules['ed_semester_.' . $i] = 'required|numeric';
+                } else if(Input::get('ed_select_degree_.' . $i) == "graduate") {
+                    $rules['ed_crea_state_.' . $i] = 'required|in:ac,al'; 
+                    $rules['ed_crea_number_.' . $i] = 'required|number';
+                }
+            }
+        }
+        return $rules;  
     }
 
     public function messages()
     {
-        return [
+        $messages = array();
+        $messages = [
             'name.required'                         => 'O campo nome é obrigatório',
             'name.min'                              => 'Mínimo de caracteres para o nome é 3',
             'name.max'                              => 'Máximo de caracteres para o nome é 100',
@@ -124,6 +144,19 @@ class ComplementInformationsRequest extends FormRequest
             'about_me.max'                          => 'Máximo de caracteres para o sobre mim é 1500',
             'image_perfil.mimes'                    => 'Insira uma imagem no formato jpeg, png ou jpg',
             'image_perfil.max'                      => 'Tamanho máximo para imagem de perfil é 2MB',
+            //Educations
+            'ed_select_degree_.*.required'          => 'O campo grau é obrigatório',
+            'ed_course_.*.required'                 => 'O campo curso é obrigatório',
+            'ed_course_.*.min'                      => 'Mínimo de caracteres para o campo curso é 3',
+            'ed_course_.*.max'                      => 'Máximo de caracteres para o campo curso é 100',
+            'ed_college_.*.required'                => 'O campo instituição de ensino é obrigatório',
+            'ed_college_.*.min'                     => 'Mínimo de caracteres para o campo instituição de ensino é 3',
+             'ed_college_.*.max'                    => 'Máximo de caracteres para o campo instituição de ensino é 3',
+             'ed_start_date_.*.required'            => 'O campo ano de início é obrigatório',
+             'ed_start_date_.*.numeric'             => 'O campo ano de início só aceita números',
+             'ed_start_date_.*.exactly'             => 'O campo ano de início tem que possuir 4 números',
+             'ed_end_date_.*.numeric'               => 'O campo ano de término só aceita números',
+             'ed_end_date_.*.exactly'               => 'O campo ano de término tem que possuir 4 números',
             //Experiences
             'ex_company_name_.*.required'           => 'O campo nome da empresa é obrigatório',
             'ex_company_name_.*.min'                => 'Mínimo de caracteres para o campo nome da empresa é 3',
@@ -136,11 +169,29 @@ class ComplementInformationsRequest extends FormRequest
             'ex_end_date_.*.numeric'                => 'O campo data de término só aceita números',
             'ex_end_date_.*.exactly'                => 'O campo data de término tem que possuir 4 números'
         ];
+        //As graduation selection applies the respective messages
+        for($i = 1; $i <= count(Input::get('ed_select_degree_.*')); $i++ ) {
+            $ed_select_degree = Input::get('ed_select_degree_.' . $i );
+
+            if(!empty($ed_select_degree)) {
+                if($ed_select_degree == "graduating") {
+                    $messages['ed_semester_.' . $i . '.required'] = "O campo semestre é obrigatório";
+                    $messages['ed_semester_.' . $i . '.numeric'] = "O campo semestre só aceita números";
+                } else if($ed_select_degree == "graduate") {
+                    $messages['ed_crea_state_.' . $i. '.required'] = "O campo estado é obrigatório"; 
+                    $messages['ed_crea_state_.' . $i. '.in'] = "Escolha um estado";
+                    $messages['ed_crea_number_.' . $i .'.required'] = "O campo CREA é obrigatório";
+                    $messages['ed_crea_number_.' . $i .'.numeric'] = "O campo CREA só aceita números";
+                }
+            }
+        }
+        return $messages;
     }
 
     public function withValidator($validator) {
         $validator->after(function ($validator) {
             session()->flash('errors_experiences','yes');
+            session()->flash('errors_educations','yes');
         });
     } 
 }
--- resources/views/site/user/education/education.blade.php
+++ resources/views/site/user/education/education.blade.php
@@ -24,29 +24,39 @@
 	@for($i = 1; $i <= $number_of_educations; $i++)
 
 		<div class="education" data-education="{{$i}}">
-			<div class="form-group">
+			<div class="form-group @if ($errors->has('ed_select_degree_.'.$i)) has-error @endif">
 				<label class="control-label" for="select_degree">Grau</label>
 				<div>
 					<label class="styled-select degree">
-						<select id="ed_select_degree" name="ed_select_degree_[{{$i}}]" class="dropdown-product selectpicker">
+						<select id="ed_select_degree_[{{$i}}]" name="ed_select_degree_[{{$i}}]" data-id="{{$i}}" class="dropdown-product selectpicker">
 							<option value="">Selecione uma opção</option>
-							<option value="graduating">Graduando</option>
-							<option value="graduate">Graduado</option>
+							<option value="graduating" 
+							{{ (old('ed_select_degree_.'.$i) == "graduating" ? "selected" : " ")}}>Graduando</option>
+							<option value="graduate" {{ (old('ed_select_degree_.'.$i) == "graduate" ? "selected" : " ")}}>Graduado</option>
 						</select>
+						@if ($errors->has('ed_select_degree_.'.$i)) 
+							<p class="alert-danger">{{ $errors->first('ed_select_degree_.'.$i) }}</p> 
+						@endif
 					</label>
 				</div>
 			</div>
-			<div class="form-group">
+			<div class="form-group @if ($errors->has('ed_course_.'.$i)) has-error @endif">
 				<label class="control-label" for="textarea">Curso</label>
-				<input type="text" class="form-control" name="ed_course_[{{$i}}]" placeholder="Curso">
+				<input type="text" class="form-control" name="ed_course_[{{$i}}]" placeholder="Curso" value="{{old('ed_course_.'.$i)}}">
+				@if ($errors->has('ed_course_.'.$i)) 
+					<p class="alert-danger">{{ $errors->first('ed_course_.'.$i) }}</p> 
+				@endif
 			</div>
-			<div class="form-group" id="semestre">
+			<div class="form-group semester @if ($errors->has('ed_semester_.'.$i)) has-error @endif" id="semester_{{$i}}">
 				<label class="control-label" for="textarea">Semestre</label>
-				<input type="text" class="form-control" name="ed_semester_[{{$i}}]" placeholder="Ex: 6°">
+				<input type="number" class="form-control" name="ed_semester_[{{$i}}]" placeholder="Ex: 6" value="{{old('ed_semester_.'.$i)}}">
+				@if ($errors->has('ed_semester_.'.$i)) 
+					<p class="alert-danger">{{ $errors->first('ed_semester_.'.$i) }}</p> 
+				@endif
 			</div>
-			<div class="add-post-btn" id="crea">
-				<div class="pull-left" id="crea-numero">
-					<div class="form-group">
+			<div class="add-post-btn crea" id="crea_{{$i}}">
+				<div class="pull-left">
+					<div class="form-group @if ($errors->has('ed_crea_state_.'.$i)) has-error @endif">
 						<label class="control-label" for="textarea">Estado</label>
 						<div class="search-category-container">
 							<label class="styled-select state">
@@ -80,31 +90,46 @@
 									<option value="sp">São Paulo</option> 
 									<option value="to">Tocantins</option> 
 								</select>
+								@if ($errors->has('ed_crea_state_.'.$i)) 
+									<p class="alert-danger">{{ $errors->first('ed_crea_state_.'.$i) }}</p> 
+								@endif
 							</label>
 						</div>
 					</div>
 				</div>
-				<div class="pull-left" id="crea-estado">
-					<div class="form-group">
+				<div class="pull-left" id="crea-number">
+					<div class="form-group @if ($errors->has('ed_crea_number_.'.$i)) has-error @endif">
 						<label class="control-label" for="textarea">Número do CREA</label>
-						<input type="text" class="form-control" name="ed_crea_number_[{{$i}}]" placeholder="Ex: 123456">
+						<input type="number" class="form-control" name="ed_crea_number_[{{$i}}]" placeholder="Ex: 123456" value="{{old('ed_crea_number_.'.$i)}}">
+						@if ($errors->has('ed_crea_number_.'.$i)) 
+							<p class="alert-danger">{{ $errors->first('ed_crea_number_.'.$i) }}</p> 
+						@endif
 					</div>
 				</div>
 				<div class="clear"> </div>
 			</div>
-			<div class="form-group">
+			<div class="form-group @if ($errors->has('ed_college_.'.$i)) has-error @endif">
 				<label class="control-label" for="textarea">Instituição de ensino</label>
-				<input type="text" class="form-control" name="ed_college_[{{$i}}]" placeholder="Ex: UniRitter">
+				<input type="text" class="form-control" name="ed_college_[{{$i}}]" placeholder="Ex: UniRitter" value="{{old('ed_college_.'.$i)}}">
+				@if ($errors->has('ed_college_.'.$i)) 
+					<p class="alert-danger">{{ $errors->first('ed_college_.'.$i) }}</p> 
+				@endif
 			</div>
 			<div class="form-group">
 				<div class="row">
-					<div class="col-md-6">
-						<label class="control-label" for="textarea">Inicio</label>
-						<input type="text" class="form-control" name="ed_start_date_[{{$i}}]" placeholder="Ex: 2014">
+					<div class="col-md-6 @if ($errors->has('ed_start_date_.'.$i)) has-error @endif">
+						<label class="control-label" for="textarea">Ano de Início</label>
+						<input type="number" class="form-control" name="ed_start_date_[{{$i}}]" placeholder="Ex: 2014" value="{{old('ed_start_date_.'.$i)}}">
+						@if ($errors->has('ed_start_date_.'.$i)) 
+							<p class="alert-danger">{{ $errors->first('ed_start_date_.'.$i) }}</p> 
+						@endif
 					</div>
-					<div class="col-md-6">
-						<label class="control-label" for="textarea">Término</label>
-						<input type="text" class="form-control" name="ed_end_date_[{{$i}}]" placeholder="Ex: 2018">
+					<div class="col-md-6 @if ($errors->has('ed_end_date_.'.$i)) has-error @endif">
+						<label class="control-label" for="textarea">Ano de término</label>
+						<input type="number" class="form-control" name="ed_end_date_[{{$i}}]" placeholder="Ex: 2018" value="{{old('ed_end_date_.'.$i)}}">
+						@if ($errors->has('ed_end_date_.'.$i)) 
+							<p class="alert-danger">{{ $errors->first('ed_end_date_.'.$i) }}</p> 
+						@endif
 					</div>
 				</div>
 			</div>
