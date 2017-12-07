c990f3e2ffa5337999d8fb3c9ef892832be1031c
toutche <contato@toutche.com.br>
2017-12-07 10:06:14 -0200

    Adicionado novo fillable para a tabela users
    Fillable atualizado para a nova coluna (is_company).

--- app/Models/Site/User/User.php
+++ app/Models/Site/User/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name','user_name', 'email', 'password','status'
+        'name','user_name', 'email', 'password','status', 'is_company'
     ];
 
     /**
